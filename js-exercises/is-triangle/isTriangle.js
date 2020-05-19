999328f3e1570866b6c4492796dd26a8a2c8e880
unknown <yalla.venky111@gmail.com>
2020-05-19 21:03:03 +0530

    third commit

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,4 +1,4 @@
 function isTriangle(s1, s2, s3) {
-  return (s1 + s2 <= s3 || s1 + s3 <= s2 || s2 + s3 <= s1);
+  return !(s1 + s2 <= s3 || s1 + s3 <= s2 || s2 + s3 <= s1);
 }
 export { isTriangle };
---
+++ js-exercises/sum-all/sumAll.js
@@ -0,0 +1,16 @@
+function calculateSum(num1, num2) {
+  let sum = 0;
+  let startNum = num1;
+  while (startNum <= num2) {
+    sum += num1;
+    startNum += 1;
+  }
+  return sum;
+}
+function sumAll(inputArray) {
+  const num1 = inputArray[0];
+  const num2 = inputArray[1];
+  if (num1 > num2) return calculateSum(num2, num1);
+  return calculateSum(num1, num2);
+}
+export { sumAll };
